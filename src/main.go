7ed93d1b0eb51baa12f28c14af45dc0da29ecd2d
mccomack <135214055+Mccomack@users.noreply.github.com>
2026-07-30 22:49:49 +0900

    input

--- src/main.go
+++ src/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var a string
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	fmt.Println(a)
 }
